f73d9e89fa2467ef089eaa0f0f12b5daf41da938
elhajuojy <souirimehdi311@gmail.com>
2022-12-07 18:21:01 +0100

    404 page 🚀 finish ❤️

--- views/404.php
+++ views/404.php
@@ -1,1 +1,35 @@
-404
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: brown;
+            color: white;
+            margin: 0;
+            padding: 0;
+            display: grid;
+            justify-content: center;
+            font-family: 'Courier New', Courier, monospace;
+        }
+        .not-found{
+            height: 100vh;
+            display: grid;
+            align-items: center;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container not-found">
+        <div class="content">
+        <h1>404</h1>
+        <p>Oops ! This Page could not be Found</p>
+        </div>
+    </div>
+    
+</body>
+</html>
