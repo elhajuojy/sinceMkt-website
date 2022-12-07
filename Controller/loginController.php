4b0c1cfa30d9cea3197a14a47af8b46c827d5688
elhajuojy <souirimehdi311@gmail.com>
2022-12-07 17:02:20 +0100

    login controller validation 🚀

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -15,38 +15,29 @@ if($_SERVER["REQUEST_METHOD"]=="GET"){
 
 }
 
-
-
 if($_SERVER["REQUEST_METHOD"]=="POST"){
    $password = $_POST['password'];
    $email = $_POST['email'];
-   
    $admin = new Admin();
-
    //todo the validation  for the inputs 
+   $admin = $admin->findWhere($email,$password);
+   
    if(func::emptyInput($password)|| func::emptyInput($email)){
     $emailerror = "email filed is empty";
     $passworderror = "password filed is empty ";
    }
-   
-   
-   $admin = $admin->findWhere($email,$password);
-
-    if(!$admin){
+    else if(!$admin){
             $emailerror ="email is not correct";
             $passworderror ="password not correct";
     }
     else{
         //todo :go to admin dashborad and create session for this login event 
-        // func::dd($admin);
         $_SESSION["email"] = $admin[0]->login;
         $_SESSION["name"]  = $admin[0]->name;
-        
         echo $_SESSION["email"];
         echo $_SESSION["name"] ;
         func::goTo("/admin");
     }
-    
 }
 require "views/login.php";
 
