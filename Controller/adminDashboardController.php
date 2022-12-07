c11f93965e67a17f169aa1df00d9d84056cfab56
elhajuojy <souirimehdi311@gmail.com>
2022-12-08 00:24:49 +0100

    add adminDashboard 🚀

--- Controller/adminDashboardController.php
+++ Controller/adminDashboardController.php
@@ -5,6 +5,14 @@ use core\func;
 func::islogedIn("email");
 
 
+
+$styles = 
+    ""
+    // '<link rel="stylesheet" href="../../assets/home.css">'
+;
+$title = "Admin Dashboard";
+
+
 require "views/adminDashboard.php";
 
 
--- views/adminDashboard.php
+++ views/adminDashboard.php
@@ -1,1 +1,15 @@
-admin dashborad
+<!-- build simple   dashborad  -->
+
+<?php
+    //todo: build simple dashboard for admin to crud product 
+?>
+
+<?php include "partials/header.php" ?>
+
+<section class="admin-container">
+    <nav>
+        <ul class="">
+            <li>SineMkt</li>
+        </ul>
+    </nav>
+</section>
