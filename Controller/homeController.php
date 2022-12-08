a039cd0d6f24cf765e62a1b5187ee16b291ba5b0
elhajuojy <souirimehdi311@gmail.com>
2022-12-08 14:47:53 +0100

    add some  path to home page 🚀🐲

--- Controller/homeController.php
+++ Controller/homeController.php
@@ -1,12 +1,13 @@
 <?php
 
-
-
-    
+use core\func;
 
 $styles = 
     '<link rel="stylesheet" href="../../assets/home.css">'
 ;
 $title = "Home";
 
+
 require "views/home.php";
+
+// func::rander("home");
