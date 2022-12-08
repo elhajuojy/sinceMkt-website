6ff1d9d424ac73d01cd6ecfb39ece7497af19aff
elhajuojy <souirimehdi311@gmail.com>
2022-12-08 19:18:22 +0100

    upload image 💖

--- core/func.php
+++ core/func.php
@@ -2,6 +2,8 @@
 
 namespace core;
 
+define("MB", 1048576);
+
 class func
 {
 
@@ -17,32 +19,61 @@ class func
     {
         return $_SERVER['REQUEST_URI'] === $value;
     }
-    
-    public static function emptyInput($value){
-        if (!empty($value) ) {
+
+    public static function emptyInput($value)
+    {
+        if (!empty($value)) {
             return  $result = false;
         }
         return $result = true;
     }
-    
-    public static function goTo(string $location, $code = 200){
-        header("location: ".$location);
+
+    public static function goTo(string $location, $code = 200)
+    {
+        header("location: " . $location);
         exit();
     }
-    public static function islogedIn($value){
-        if(!isset($_SESSION[$value]))
-        {   
+    public static function islogedIn($value)
+    {
+        if (!isset($_SESSION[$value])) {
             self::goTo("/");
         }
     }
-    public static function logOut(){
+    public static function logOut()
+    {
         session_start();
         session_unset();
         session_destroy();
         self::goTo("/");
     }
-    public static function rander($path){
-        require_once "views/".$path.".php";
-    }
     
+    public static function rander($path)
+    {
+        require_once "views/" . $path . ".php";
+    }
+
+    public function imageUpload($imageRequest)
+    {
+        global $msgError;
+        $imagename  = rand(1000, 10000) . $_FILES[$imageRequest]['name'];
+        $imagetmp   = $_FILES[$imageRequest]['tmp_name'];
+        $imagesize  = $_FILES[$imageRequest]['size'];
+        $allowExt   = array("jpg", "png", "gif", "mp3", "pdf");
+        $strToArray = explode(".", $imagename);
+        $ext        = end($strToArray);
+        $ext        = strtolower($ext);
+
+        if (!empty($imagename) && !in_array($ext, $allowExt)) {
+            $msgError = "EXT";
+        }
+        if ($imagesize > 2 * MB) {
+            $msgError = "size";
+        }
+        if (empty($msgError)) {
+            move_uploaded_file($imagetmp,  "../upload/" . $imagename);
+            return $imagename;
+        } else {
+            return "fail";
+        }
+    }
 }
