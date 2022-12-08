305cf3ac854af24b49afca1ec1a98f0a25ddb518
elhajuojy <souirimehdi311@gmail.com>
2022-12-08 14:48:09 +0100

    admin dashboard 👩🏿‍🎓

--- Controller/adminDashboardController.php
+++ Controller/adminDashboardController.php
@@ -8,12 +8,22 @@ func::islogedIn("email");
 
 
 $styles = 
-    ""
+    '
+          <link
+          rel="stylesheet"
+          href="assets/Admin/assets/bootstrap/css/bootstrap.min.css?h=025df1ec88740cad5ff14bb3380da6dd"
+        />
+
+        <link
+          rel="stylesheet"
+          href="assets/Admin/assets/css/styles.min.css?h=f3f5e3e0fd9b38313d60c6777e01a15a"
+        />
+    '
     // '<link rel="stylesheet" href="../../assets/home.css">'
 ;
 $title = "Admin Dashboard";
 
-
-require "views/adminDashboard.php";
+require "views/admindashboard.php";
+// func::rander("adminDashboard");
 
 
