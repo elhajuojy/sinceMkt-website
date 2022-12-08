980eca0d20c7efd394f45885db1e8c55ed3e9369
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2022-12-08 23:23:53 +0100

    List Product Controller 💖

---
+++ Controller/ListProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+use core\func;
+use Model\Products;
+
+func::islogedIn("email");
+
+$script = '<script src="../../assets/Product/js/listproduct.js"></script>';
+
+$styles =
+    '
+    <link
+        rel="stylesheet"    
+        href="../../assets/Product/assets/bootstrap/css/bootstrap.min.css"
+        />
+        <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        />
+
+        <link
+        rel="stylesheet"
+        href="../../assets/Product/assets/ProductList.css"
+        />
+        
+        
+    '
+    // '<link rel="stylesheet" href="../../assets/home.css">'
+;
+
+
+if (isset($_POST["logout"])) {
+    func::logOut();
+}
+
+$productList = (new Products())->findAll();
+$title = "List Product";
+
+if (isset($_GET["displaySend"])){
+    require "views/ProductList.php";
+    die();
+}
+
+if(isset($_POST) && isset($_POST["DELETE"])){
+    $product = new Products();
+
+    $product->removeById($_POST["id"]);
+    func::goTo("/admin/listproduct");
+}
+
+
+
+require "views/ProductList.php";
