08e1965ea1c8e6b392560350889fa0aae7add665
elhajuojy <souirimehdi311@gmail.com>
2023-01-31 10:01:05 +0100

    update database problems

--- assets/sinemkt_store_query.sql
+++ assets/sinemkt_store_query.sql
@@ -1,23 +1,32 @@
--- Active: 1670403516212@@localhost@3306@sinemkt_store
+-- Active: 1675154731798@@127.0.0.1@3306@sinemkt_store
+
+DROP DATABASE IF EXISTS sinemkt_store;
+CREATE  DATABASE sinemkt_store;
 
 --Query 🚀
+USE sinemkt_store;
+
 CREATE TABLE Admin(  
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT ,
-    login TEXT,
-    email TEXT,
-    name TEXT,
-    password VARCHAR,
-    role TEXT
+    login VARCHAR(255),
+    email VARCHAR(255),
+    name VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(255)
 ) ;
 
+INSERT INTO Admin (login, email, name, password, role) VALUES ('admin', 'admin', 'admin', 'admin', 'admin');
 
 
-CREATE TABLE Product(  
+DROP TABLE IF EXISTS Products;
+
+CREATE TABLE Products (  
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT ,
-    title TEXT,
-    description TEXT,
-    price FLOAT,
-    image TEXT,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    price  FLOAT,
+    image VARCHAR(255)
 ) ;
 
-SELECT * from products;
+
+-- SELECT * from products;
